6f12bd35036503fb39c8f24b98110a714b30dc86
Fab90awuiee <166625400+Fab90awuiee@users.noreply.github.com>
2025-11-08 12:19:26 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.program-line__title').forEach(title => {
+    title.addEventListener('click', function () {
+        const currentDescr = this.nextElementSibling;
+
+        if (!currentDescr.classList.contains('active')) {
+            document.querySelectorAll('.program-line__descr.active').forEach(descr => {
+                descr.classList.remove('active');
+            });
+
+            currentDescr.classList.add('active');
+        }
+    });
+});
